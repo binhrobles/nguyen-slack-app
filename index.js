bd6f3739d22a959a7f4d788c04b2ec1f3731ddd1
Binh 🌱 <binhrobles@gmail.com>
2022-08-14 16:14:08 -0400

    echo responses back

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
 import 'dotenv/config';
 import Bolt from '@slack/bolt';
 
-import { isParent } from './utils.js';
+import { isParent, respondInThread } from './utils.js';
+
+const SLACK_SIGNING_SECRET = process.env.SLACK_SIGNING_SECRET;
+const SLACK_BOT_TOKEN = process.env.SLACK_BOT_TOKEN;
 
 const app = new Bolt.App({
-	signingSecret: process.env.SLACK_SIGNING_SECRET,
-	token: process.env.SLACK_BOT_TOKEN,
+	signingSecret: SLACK_SIGNING_SECRET,
+	token: SLACK_BOT_TOKEN,
 });
 
-app.event('message', async ({ message, client }) => {
+app.message(async ({ message }) => {
 	try {
-		console.dir(message);
-
 		// only want to do translations on parent messages
 		if (isParent(message)) {
-			console.log('parent');
+			console.log('heard parent');
+
+			await respondInThread({
+				app,
+				message,
+				text: `:speaking_head_in_silhouette: ${message.text}`,
+			});
 		}
 	} catch (e) {
 		console.error(e);
--- utils.js
+++ utils.js
@@ -3,3 +3,13 @@
 // the message itself.
 // https://api.slack.com/messaging/retrieving#finding_threads
 export const isParent = ({ thread_ts, ts }) => !thread_ts || thread_ts === ts;
+
+export const respondInThread = async ({ app, message, text }) => {
+	await app.client.chat.postMessage({
+		token: app.token,
+		text,
+		channel: message.channel,
+		thread_ts: message.ts,
+	});
+}
+
