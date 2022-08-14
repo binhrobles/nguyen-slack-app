dabaf60310e55ea23b3137b51f5d3d71bc2ffae0
Binh 🌱 <binhrobles@gmail.com>
2022-08-14 15:32:40 -0400

    add bolt

---
+++ index.js
@@ -0,0 +1,16 @@
+import 'dotenv/config'
+import Bolt from '@slack/bolt';
+
+const app = new Bolt.App({
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  token: process.env.SLACK_BOT_TOKEN,
+});
+
+/* Add functionality here */
+
+(async () => {
+  // Start the app
+  await app.start(process.env.PORT || 3000);
+
+  console.log('⚡️ Bolt app is running!');
+})();
