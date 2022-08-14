6f699da7d02aa86323ea8ab86442257a650c378f
Binh 🌱 <binhrobles@gmail.com>
2022-08-14 16:38:44 -0400

    Translate English to Viet

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 import 'dotenv/config';
-import Bolt from '@slack/bolt';
 
-import { isParent, respondInThread } from './utils.js';
+// Slack configuration
+// https://slack.dev/bolt-js/reference
+import Bolt from '@slack/bolt';
 
 const SLACK_SIGNING_SECRET = process.env.SLACK_SIGNING_SECRET;
 const SLACK_BOT_TOKEN = process.env.SLACK_BOT_TOKEN;
@@ -11,16 +12,33 @@ const app = new Bolt.App({
 	token: SLACK_BOT_TOKEN,
 });
 
+// AWS Translate configuration
+// https://docs.aws.amazon.com/AWSJavaScriptSDK/v3/latest/clients/client-translate/index.html
+import { TranslateClient, TranslateTextCommand } from "@aws-sdk/client-translate";
+const translateClient = new TranslateClient({ region: 'us-west-2' });
+
+// App logic
+
+import { isParent, respondInThread } from './utils.js';
+
 app.message(async ({ message }) => {
 	try {
 		// only want to do translations on parent messages
 		if (isParent(message)) {
 			console.log('heard parent');
 
+			// TODO: need to detect english vs viet
+
+			const translation = await translateClient.send(new TranslateTextCommand({
+				SourceLanguageCode: 'en',
+				TargetLanguageCode: 'vi',
+				Text: message.text,
+			}));
+
 			await respondInThread({
 				app,
 				message,
-				text: `:speaking_head_in_silhouette: ${message.text}`,
+				text: `:ba: ${translation.TranslatedText}`,
 			});
 		}
 	} catch (e) {
